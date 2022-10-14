01044c1dd7ac18083c9c526b68e54a6f527c9e7a
gabriel.arias <gabriel.arias.ok@gmail.com>
2022-10-14 17:47:27 -0300

    Se realiza la estetica del form. Resta ver etiquetas y procesos

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 function sendWpp(){
     window.open('https://api.whatsapp.com/send?phone=5491123204371&text=Hola! Me gustaría saber más sobre codeninja');
 };
+
+function sendConsulta(){
+    window.open('mailto:equipo.codeninja@gmail.com?subject=subject&body=body');
+    //La idea es reemplazar este fragmento de código por un servicio de envio de mail
+}
