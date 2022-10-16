70bf28ed3f9fec72299786079d4d44778a8c7620
gabriel.arias <gabriel.arias.ok@gmail.com>
2022-10-16 14:11:59 -0300

    Se hace la validacion de campos del formulario. No funciona correctamente el regex del mail

--- scripts.js
+++ scripts.js
@@ -3,24 +3,40 @@ function sendWpp(){
 };
 
 function sendConsulta(){
-    let nya = document.getElementById('formNombre').innerHTML;
-    let mail = document.getElementById('formMail').innerHTML;
-    let body = document.getElementById('formConsulta').innerHTML;
 
-    console.log(nya);
-    console.log(mail);
-    console.log(body);
-    
-    Email.send({
-          Host: "smtp.gmail.com",
-          Username: "equipo.codeninja@gmail.com",
-          Password: "Prueba!123",
-          To: 'equipo.codeninja@gmail.com',
-          From: 'gabriel.arias.ok1@gmail.com',
-          Subject: "Consulta de ",
-          Body: 'body de prueba',
-        })
-          .then(function (message) {
-            alert("mail sent successfully")
-          });
+}
+
+function validarDatos(){
+   let nombre = document.getElementById("formNombre").value;
+   let mail = document.getElementById("formMail").value;
+   
+   //Patron para utilizar al validar nombre(solo letras Mm)
+   const patternNombre = new RegExp('^[A-Z ]+$', 'i');
+   //Patron para utilizar al validar mail
+   const patternMail = new RegExp('/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i');
+
+   //valido nombre
+   if(nombre.length <= 50){
+      if(!patternNombre.test(nombre))
+      {
+        alert("El nombre debe contener letras solamente");
+        document.getElementById("formNombre").value = "";
+        document.envioConsulta.formNombre.focus();
+        return 0;    
+      }
+   }else{
+    alert("El nombre no puede exceder los 50 caracteres");
+    document.getElementById("formNombre").value = "";
+    document.envioConsulta.formNombre.focus();
+    return 0;
+   }
+
+   //valido mail
+   if(!patternMail.test(mail)){
+        alert("El mail ingresado no es correcto");
+        document.getElementById("formMail").value = "";
+        document.envioConsulta.formMail.focus();
+        return 0;  
+   }
+
 }
